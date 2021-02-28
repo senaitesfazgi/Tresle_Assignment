d2e1a9b805b9d2348864d84fc772eb7e1022315e
senaitesfazgi <61923461+senaitesfazgi@users.noreply.github.com>
2021-02-28 12:31:20 -0700

    added model and controller

---
+++ app/Http/Controllers/TresleController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Tresle;
+use Illuminate\Http\Request;
+
+class TresleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Tresle  $tresle
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Tresle $tresle)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Tresle  $tresle
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Tresle $tresle)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Tresle  $tresle
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Tresle $tresle)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Tresle  $tresle
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Tresle $tresle)
+    {
+        //
+    }
+}
