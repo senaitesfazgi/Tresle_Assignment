2e510a2e9f1005d8404a0934bd766265bd85bbd2
senaitesfazgi <61923461+senaitesfazgi@users.noreply.github.com>
2021-02-28 15:52:08 -0700

    added create business form

---
+++ resources/views/tresle/create.blade.php
@@ -0,0 +1,53 @@
+@extends('tresle.layout')
+  
+@section('content')
+<div class="row">
+    <div class="col-lg-12 margin-tb">
+        <div class="pull-left">
+            <h2>Create New Business</h2>
+        </div>
+        <div class="pull-right">
+            <a class="btn btn-primary" href="{{ route('tresle.index') }}"> Back</a>
+        </div>
+    </div>
+</div>
+   
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <strong>Warning!</strong> Please check your input code<br><br>
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+   
+<form action="{{ route('tresle.store') }}" method="POST">
+    @csrf
+     <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Business Name:</strong>
+                <input type="text" name="businessName" class="form-control" placeholder="Business Name">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Business Price:</strong>
+                <input type="text" name="businessPrice" placeholder="Business Price">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Business City:</strong>
+                <input type="text" name="businessCity" placeholder="Business City">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+                <button type="submit" class="btn btn-primary">Submit</button>
+        </div>
+    </div>
+   
+</form>
+@endsection
